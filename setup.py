1687807816334c8a024587f907ea21214b3b9d47
Jaume Zaragoza <jzaragoza@prompsit.com>
2023-05-30 10:44:47 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     setuptools.setup(
         name="monotextor",
-        version="1.0.0",
+        version="1.1.0",
         install_requires=requirements,
         license="GNU General Public License v3.0",
         #author=,
